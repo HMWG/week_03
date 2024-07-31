c45f3fb06594ec8c098115e727e75c0a5a5cf57a
HMWG <nurung22222@gmail.com>
2024-07-31 15:37:46 +0900

    First Commit

---
+++ src/main/java/org/example/OrderRepository.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+
+public interface OrderRepository {
+    public void save();
+
+    public void delete(int order_id);
+
+    public void update(int order_id);
+
+    public void findByOrderId();
+
+    public void findAll();
+
+    public void findByTotalPrice(int total_price);
+}
