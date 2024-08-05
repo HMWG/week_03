a3a0303b5f02f397f883d7578cb7cbf3de39971b
강지원 <kangji149@naver.com>
2024-08-05 15:04:57 +0900

    chore : fix name

---
+++ src/main/java/org/example/domain/order/service/CreateOrderService.java
@@ -0,0 +1,18 @@
+package org.example.domain.order.service;
+
+import java.util.List;
+import org.example.domain.order.BasketItem;
+import org.example.domain.order.Order;
+import org.example.domain.product.Product;
+
+public class CreateOrderService {
+
+    Order createOrder(Product product, int quantity) {
+
+        return null;
+    }
+
+    Order createOrder(List<BasketItem> basketItems) {
+        return null;
+    }
+}
