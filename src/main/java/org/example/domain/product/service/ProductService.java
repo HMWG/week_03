1ded7603c76d8a6dd90d44753b2fab1af4dc2f6e
icecoff22 <icecoff2222@gmail.com>
2024-08-05 16:03:41 +0900

    feat : 상품, 주문 서비스 기능 추가

--- src/main/java/org/example/domain/product/service/ProductService.java
+++ src/main/java/org/example/domain/product/service/ProductService.java
@@ -1,2 +1,43 @@
-package org.example.domain.product.service;public class ProductService {
+package org.example.domain.product.service;
+
+import org.example.domain.order.Order;
+import org.example.domain.order.OrderStatus;
+import org.example.domain.order.repository.OrderRepositoryImpl;
+import org.example.domain.order.service.OrderRepository;
+import org.example.domain.product.Product;
+import org.example.domain.product.repository.ProductRepositoryImpl;
+
+import java.util.List;
+
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+
+    public ProductService() {
+        this.productRepository = new ProductRepositoryImpl();
+        this.orderRepository = new OrderRepositoryImpl();
+    }
+
+    public void registerProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(long productId) {
+        productRepository.deleteById(productId);
+    }
+
+    public List<Order> getPayCompleteOrders() {
+        return orderRepository.findAll().stream()
+                .filter(o -> o.getOrderStatus().equals(OrderStatus.PAY_COMPLETE)).toList();
+    }
+
+    public void acceptDelivery(List<Order> payCompleteOrders, Integer index) {
+        Order order = payCompleteOrders.get(index);
+        order.setOrderStatus(OrderStatus.SHIPPING);
+        orderRepository.update(order);
+    }
+
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
 }
